3c37ad7268b0f391b2edd21fb9ae8c2cd40cd351
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2020-01-17 11:58:53 +0100

    create PersonSelectorFormInput
    + create PersonSelector for Formik
    + add export from directory index
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/main/react/components/selector/index.js
+++ src/main/react/components/selector/index.js
@@ -1,3 +1,4 @@
 export {UserSelector} from "./UserSelector"
 export {PersonSelector} from "./PersonSelector"
 export {UserSelectorFormInput} from "./UserSelectorFormInput"
+export {PersonSelectorFormInput} from "./PersonSelectorFormInput"
